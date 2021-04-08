e363cca463ad62e0baf625aed9a475ea9a4b3bd7
0elle0 <el_leo_m@hotmail.com>
2021-04-08 21:24:51 +0200

    installed +deps

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import {Platform} from 'react-native'
+import {Ionicons} from "@expo/vector-icons";
 
 //navigation imports
-
+import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack';
+import {createDrawerNavigator} from "react-navigation-drawer";
+import {createMaterialBottomTabNavigator} from "react-navigation-material-bottom-tabs";
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+//own imports
+import Colors from "../constants/Colors";

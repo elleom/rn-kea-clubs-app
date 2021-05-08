1484b48fe8438c9373b8be8b7cc2b9fa2e758a90
0elle0 <el_leo_m@hotmail.com>
2021-05-08 12:43:16 +0200

    loaded AsysnFont + AppLoading

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import MainNavigator from "./navigation/MainNavigator";
+import * as Font from 'expo-font';
+import AppLoading from "expo-app-loading";
+
+
+const fetchFont = () => {
+    return (
+        Font.loadAsync(
+            {
+                'oxanium-regular': require('./assets/fonts/Oxanium-Regular.ttf')
+            }
+        )
+    )
+
+}
 
 export default function App() {
-  return (
-    <MainNavigator />
-  );
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFont}
+                onFinish={() => setFontLoaded(true)}
+                onError={(error) => console.log(error)}
+            />)
+    }
+
+    return (
+        <MainNavigator/>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });

9ba0bb01af1e93ea25cd4b1db50bd682d282ad32
elleom <el_leo_m@hotmail.com>
2021-08-04 20:49:43 +0200

    add Date Picker

--- screens/User/AddEditEventScreen.js
+++ screens/User/AddEditEventScreen.js
@@ -1,16 +1,84 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native'
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, ScrollView, KeyboardAvoidingView, TextInput, Platform} from 'react-native'
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/CustomHeaderButton";
 import Colors from "../../constants/Colors";
+
 import {EVENTS} from "../../data/dummy-data";
+import DateRangePicker from "rnv-date-range-picker";
+import moment from "moment";
 
 const AddEditEventScreen = props => {
     const eventId = props.navigation.getParam('eventId')
+    const [selectedRange, setRange] = useState({});
+
     return (
-        <View>
-            <Text>{eventId}</Text>
-        </View>
+        <KeyboardAvoidingView style={{flex: 1}} behavior={"padding"} keyboardVerticalOffset={100}>
+            <ScrollView>
+                <View style={styles.form}>
+                    <Text>Event title</Text>
+                    <TextInput
+                        id="title"
+                        label="Event name"
+                        errorText="Please enter a valid title!"
+                        keyboardType="default"
+                        autoCapitalize="sentences"
+                        autoCorrect
+                        returnKeyType="next"
+                        onInputChange={() => {}}
+                        initialValue={() => {}}
+                        initiallyValid={() => {}}
+                        required
+                    />
+                    <Text>Image URL</Text>
+                    <TextInput
+                        id="imageUrl"
+                        label="Image Url"
+                        errorText="Please enter a valid image url!"
+                        keyboardType="default"
+                        returnKeyType="next"
+                        onInputChange={() => {}}
+                        initialValue={() => {}}
+                        initiallyValid={() => {}}
+                        required
+                    />
+
+                    <DateRangePicker
+                        onSelectDateRange={(range) => {
+                            setRange(range);
+                        }}
+                        responseFormat="YYYY-MM-DD"
+                        minDate={moment()}
+                        maxDate={moment().add(30, "days")}
+                    />
+
+                    <View style={styles.container}>
+                        <Text>first date: {selectedRange.firstDate}</Text>
+                        <Text>second date: {selectedRange.secondDate}</Text>
+                    </View>
+
+
+
+                    <Text>Description</Text>
+                    <TextInput
+                        id="description"
+                        label="Description"
+                        errorText="Please enter a valid description!"
+                        keyboardType="default"
+                        autoCapitalize="sentences"
+                        autoCorrect
+                        multiline
+                        numberOfLines={3}
+                        // onInputChange={inputChangeHandler}
+                        // initialValue={editedProduct ? editedProduct.description : ''}
+                        // initiallyValid={!!editedProduct}
+                        required
+                        minLength={5}
+                    />
+
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 

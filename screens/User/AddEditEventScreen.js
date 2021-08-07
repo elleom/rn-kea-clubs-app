137d88b32814b816ecb498fc0d065ac1dc6460c2
elleom <el_leo_m@hotmail.com>
2021-08-08 01:04:24 +0200

    add correct input timepicker

--- screens/User/AddEditEventScreen.js
+++ screens/User/AddEditEventScreen.js
@@ -9,6 +9,9 @@ import DateRangePicker from "rnv-date-range-picker";
 import moment from "moment";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import {Ionicons} from "@expo/vector-icons";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+
+
 
 const AddEditEventScreen = props => {
     const eventId = props.navigation.getParam('eventId')
@@ -23,23 +26,79 @@ const AddEditEventScreen = props => {
     const [showEnd, setShowEnd] = useState(false);
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
+    const onSelectDateHandler = (range) => {
+        // const day = parseInt((range.firstDate.slice(0,2)));
+        // const month = parseInt(range.firstDate.slice(3,5));
+        // const year = parseInt(range.firstDate.slice(6));
+        // console.log(day)
+        // console.log(month)
+        // console.log(year)
+        // const newStartDate = new Date(year, month, day )
+        // const mockDate = new Date(2021,8,6)
+        // console.log(mockDate);
+        const date = moment(range.firstDate, "DD-MM-YYYY").toDate()
+        console.log(date)
+        console.warn(date)
+
+        setRange(range);
+    }
+
     const showDatePicker = () => {
         setDatePickerVisibility(true);
     };
 
-    const hideDatePicker = () => {
+    const hideDatePicker = (date) => {
         setDatePickerVisibility(false);
     };
 
     const handleConfirm = (date) => {
+        setStartDate(date);
+        console.log(date);
         console.warn("A date has been picked: ", date);
         hideDatePicker();
     };
 
     /**
      * form state
+     * if evenObject is true/defined then loads the date, else generates new initial value
      */
     const [title, setTitle] = useState(eventObject ? eventObject.title : '')
+    const [imageUrl, setImageUrl] = useState(eventObject ? eventObject.image : '')
+    const [type, setType] = useState(eventObject ? eventObject.type : '')
+    const [description, setDescription] = useState(eventObject ? eventObject.description : '')
+    const [startDate, setStartDate] = useState(eventObject ? eventObject.startDate : new Date())
+    const [endDate, setEndDate] = useState(eventObject ? eventObject.endDate : new Date())
+    const [location, setLocation] = useState(eventObject ? eventObject.location : '')
+    const [organization, setOrganization] = useState(eventObject ? eventObject.organization : '')
+    const [evetTimeDetails, setEventTimeDetails] = useState('')
+
+
+
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+        console.warn(currentDate)
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+
+
 
     return (
         <KeyboardAvoidingView style={{flex: 1}} behavior={"padding"} keyboardVerticalOffset={100}>
@@ -55,7 +114,8 @@ const AddEditEventScreen = props => {
                         value={title}
                         autoCorrect
                         returnKeyType="next"
-                        onInputChange={() => {}}
+                        onInputChange={() => {
+                        }}
                         initialValue={() => {
                         }}
                         initiallyValid={() => {
@@ -74,41 +134,41 @@ const AddEditEventScreen = props => {
                         required
                     />
 
-                    <DateRangePicker
-                        onSelectDateRange={(range) => {
-                            setRange(range);
-                        }}
-                        responseFormat="YYYY-MM-DD"
-                        minDate={moment()}
-                        maxDate={moment().add(30, "days")}
-                    />
+                    {/*<DateRangePicker*/}
+                    {/*    onSelectDateRange={onSelectDateHandler}*/}
+
+                    {/*    responseFormat="DD-MM-YYYY"*/}
+                    {/*    minDate={moment()}*/}
+                    {/*    maxDate={moment().add(30, "days")}*/}
+                    {/*/>*/}
 
 
-                    <View style={styles.container}>
-                        <Text>first date: {selectedRange.firstDate}</Text>
-                        <Text>second date: {selectedRange.secondDate}</Text>
-                    </View>
+                    {/*<View style={styles.container}>*/}
+                    {/*    <Text>first date: {selectedRange.firstDate}</Text>*/}
+                    {/*    <Text>second date: {selectedRange.secondDate}</Text>*/}
+                    {/*</View>*/}
+
                     <View style={styles.eventTimeContainer}>
+                        <Text>Start</Text>
                         <View style={styles.eventTime}>
-                            <Text>Start</Text>
-                            <Ionicons name='timer-outline' size={40} onPress={showDatePicker}/>
-                            <DateTimePickerModal
-                                isVisible={isDatePickerVisible}
-                                mode="time"
-                                onConfirm={handleConfirm}
-                                onCancel={hideDatePicker}
-                            />
+
+                            <Ionicons name='calendar-outline' size={32} onPress={showDatepicker}/>
+                            <Ionicons name='timer-outline' size={32} onPress={showTimepicker}/>
+
                         </View>
                         <View>
-                            <Text>Start</Text>
-                            <Ionicons name='timer-outline' size={40} onPress={showDatePicker}/>
-                            <DateTimePickerModal
-                                isVisible={isDatePickerVisible}
-                                mode="time"
-                                onConfirm={handleConfirm}
-                                onCancel={hideDatePicker}
-                            />
+                            <Button onPress={showTimepicker} title="Show time picker!" />
                         </View>
+                        {show && (
+                            <RNDateTimePicker
+                                testID="dateTimePicker"
+                                value={date}
+                                mode={mode}
+                                is24Hour={true}
+                                display="default"
+                                onChange={onChange}
+                            />
+                        )}
                     </View>
 
 

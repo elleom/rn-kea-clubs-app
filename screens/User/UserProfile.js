61ab6120f18680a43a21636c1ac34b2774d24ee7
elleom <el_leo_m@hotmail.com>
2022-01-07 11:15:41 +0100

    added Auth LOgOut and token

--- screens/User/UserProfile.js
+++ screens/User/UserProfile.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Button, Text} from 'react-native';
 import ImageSelector from "../../components/UI/ImageSelector";
 
 const UserProfile = props => {
+    const [selectedImage, setSelectedImage] = useState();
+
+    const imageTakenHandler = imagePath => {
+        setSelectedImage(imagePath)
+    }
+
     return <View>
-        <ImageSelector />
+        <ImageSelector navData={props.navigation} onImageTaken={imageTakenHandler} />
     </View>
 
 }
